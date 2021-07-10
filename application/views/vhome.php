48441ffb3918475cf972e6974260d0e27b007fce
MuhSyah <87221623+MuhSyah@users.noreply.github.com>
2021-07-10 14:14:41 +0700

    Add files via upload

---
+++ application/views/vhome.php
@@ -0,0 +1,430 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+  
+        
+    <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <meta name="description" content="">
+     <meta name="author" content="">
+
+        <title>  SARINA ELEKTRO   </title>
+        
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    
+        <style type="text/css">
+      *{
+
+     padding:0;
+    margin:0; 
+    font-family: sans-serif;
+    
+}
+a {
+     color: inherit;
+     text-decoration: none;
+    
+}
+.medsos {
+    padding:5px 0;
+    background-color: #0DC9F9;
+} 
+.medsos ul li{
+    display : inline-block;
+    color: #fff;
+    margin-right: 0px;
+    margin-left: 10px;
+}
+
+
+.container {
+    width: 100%;
+   
+    
+    }
+.containerF {
+    width: 80%;
+    margin:5px auto;
+ 
+    }
+
+
+.containerA {
+    width: 80%;
+    margin:5px auto;
+    margin-top: 5px;
+    
+
+}
+.containerT {
+    width: 80%;
+    margin:5px auto;
+    margin-top: -70px;
+
+}
+
+.container::after{
+    content:'';
+    display:block ;
+    clear: both;
+
+}
+
+
+header h1 {
+    
+    float: left;
+    padding:15px 0;
+    color:  #0DC9F9;
+}
+header ul {
+   
+    float: right; 
+    
+}
+
+header ul li {
+ display:inline-block;
+ margin:0px 5px;
+}
+
+
+header ul li a{
+   
+    padding:25px 20px;
+    display:inline-block;
+
+}
+header ul li a:hover{
+     background-color:  #0DC9F9;
+     color:#fff;
+  
+}
+.active{
+     background-color: #0DC9F9;
+    color:#fff;
+    
+}
+.banner{
+    height:62vh;
+    
+    background-image:url(img/benner.jpg);
+    background-size:cover;
+    position :relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+
+.banner:after {
+    content: '';
+    display:block;
+    position:absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(15, 190, 235, .6)
+}
+section{
+    padding:80px 0;
+  
+}
+.banner h2{
+    z-index: 1;
+    color: seashell;
+    padding: 20px 25px;
+    border:3px solid #fff;
+    font-size: 40PX;
+
+}
+
+section h3{
+    text-align: center;
+    padding: 20px 0;
+    color: #0DC9F9 ;
+    font-size: 25px;
+    margin-bottom: 20px;
+}
+.about p{
+    color: #666;
+    word-spacing: 2px;
+    line-height: 25px;
+    font-size:20px;
+   
+    
+}
+.about{
+    
+    margin-bottom:-50px;
+}
+
+
+
+
+.box{
+  
+    color:#0DC9F9;
+    margin-left: 47px;
+    
+
+}
+.box::after{
+    clear: both;
+}
+
+.box .ca1{
+    width: 250px;
+    padding: 10px;
+    box-sizing: border-box;
+    text-align: center;
+    float: left;
+    margin-left: 20px;
+    margin-top: 50px;
+    
+    }
+
+.medsos .container .a{
+    margin-left:300px;
+  
+}
+
+
+
+.icon{
+    width: 80px;
+    height: 80px;
+    border: 3px solid;
+    border-radius: 50%;
+    text-align: center;
+    line-height: 70px;
+    font-size: 50px;
+    margin: 0 auto;
+    color:#333;
+    
+}
+
+.box .ca1 h4{
+    margin: auto;
+    margin-top: 20px;
+    color:#333;
+    margin-bottom:130px;
+    
+}
+.box .ca1 h3{
+    margin: auto;
+    margin-top: 10px;
+    padding-left: 100x;
+    color:#333;
+    
+}
+.containerF h4{
+    text-align: center;
+    padding: 20px 0;
+    color: #0DC9F9 ;
+    font-size: 25px;
+    margin-bottom: 20px;
+    MARGIN-TOP: 300PX;
+}
+
+
+footer{
+    padding: 30px 0;
+    background-color: #333;
+    color:#fff;
+    text-align: center;
+}
+
+.label{
+    background-color:  #0DC9F9;
+    color: #fff;
+    font-size:30px ;
+    
+    
+}
+
+@media screen and (max-width:680px){
+
+#maincontent{
+    width: auto;
+    float: none;
+}
+#sidebar{
+    width: auto;
+    float: none;
+}
+.container{
+    width:100%
+}
+.banner h2{
+    
+    color: seashell;
+    padding: 10px 15px;
+   font-size: 20pX;
+   text-align:center;
+
+}
+
+.banner{
+    height:30vh;
+    background-size:cover;
+    background-position: center top; 
+  
+    display: flex;
+    justify-content: center;
+    align-items: left;
+}
+
+.box .ca1{
+    width: 250px;
+    padding: 10px;
+    box-sizing: border-box;
+    text-align: center;
+    float: none;
+    margin-left: -30px;
+    margin-top: 50px;
+  
+    }
+    .containerF h4{
+ 
+    margin-bottom: 20px;
+    MARGIN-TOP: 20PX;
+}
+
+    .about p{
+        font-size:13px;
+        text-align:center;
+    }
+    .icon{
+    width: 60px;
+    height: 60px;
+    text-align: center;
+    font-size: 30px;
+    margin: 0 auto;
+    color:#333;
+    
+}
+iframe{
+        height:400px;
+    width:300px;
+}
+ 
+}
+
+}
+
+
+        </style>
+     </head>
+
+    <body>
+ 
+        <!---heade=---> 
+        <div class="medsos">
+            <div class="container">
+                <ul>
+                <li><a href="https://web.facebook.com/susahmikir.namabaru"><i class="fab fa-facebook-square"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></i></a></li>
+                    <li><a href="https://api.whatsapp.com/send?phone=6285242700383"><i class="fab fa-whatsapp"></i></a></li> 
+                
+                </ul>
+            </div>
+        </div>
+
+        
+
+        <header>
+         
+
+   
+
+        </header>
+        <!---BANNER-->
+        
+        <section class="banner">
+            <h2>WELCOME TO SARINA ELEKTRO SOPPENG WEBSITE</h2>
+            
+        </section>
+        <!---ABOUT--->
+        <section class="about">
+            <div class="containerF">
+                <h3>ABOUT SARINA ELEKTRONIK</h3>
+                <p>  SARINA ELEKTRONIK menerima jasa service AC, KULKAS, TV, FREEZER, DLL. </P><P>SARINA ELEKTRONIK menyediakan teknisi yang berpengalaman di bidangnya ,
+                    Kurang lebih 30thn bekerja dibidang elektronik dan beberapa kali mengikuti asosiasi dan pelatihan dibidang elektronik.
+                     <strong> Teknisi juga telah dilengkapi dengan Sertifikasi BNSP (Badan Nasional Sertifikasi Profesi).</strong>
+                      <strong>BNSP </strong> adalah sebuah lembaga independen yang dibentuk pemerintah untuk melaksanakan ketentuan Pasal 18 ayat (5) 
+                     Undang-Undang Nomor 13 Tahun 2003 
+                     tentang Ketenagakerjaan. Badan ini bekerja untuk menjamin mutu kompetensi dan pengakuan tenaga kerja pada seluruh sektor bidang 
+                     profesi di Indonesia melalui proses sertifikasi kompetensi kerja.
+                     </p>
+        </section>
+
+        <!---service-->
+   
+
+        
+
+        <section class="contact">
+        <div class="containerF">
+        <h3>CONTACT</h3>
+
+        <div class="box">
+             <div class="ca1">
+            <div class="icon"><i class="fas fa-map-marked"></i></div>
+            <h3>Addres</h3>
+            <p></p>
+            <p> Jl. Kemakmuran No.32, Lemba, Lalabata, Kabupaten Soppeng, Sulawesi Selatan 90811</p>
+        </div>
+        <div class="ca1">
+            <div class=icon><a href="https://api.whatsapp.com/send?phone=6285242700383&text=Assalamu%27alaikum%20pak%21%20AC%20saya%20sedang%20bermasalah%0ANama%3A%0AAlamat%3A%0ANomor%20hp%3A%0AMerek%20AC%3A%0AKendala%3A"><i class="fab fa-whatsapp"></i></a></div>
+           <h3>Whatsapp</h3>
+            <p></p>
+            <a href="https://api.whatsapp.com/send?phone=6285242700383&text=Assalamu%27alaikum%20pak%21%20AC%20saya%20sedang%20bermasalah%0ANama%3A%0AAlamat%3A%0ANomor%20hp%3A%0AMerek%20AC%3A%0AKendala%3A">0852-4270-0383</a>
+        </div>
+        <div class="ca1">
+            <div class="icon"><a href="tel:+6285 242 700 383"><i class="fas fa-phone-alt"></i></a></div>
+            <h3>Telepon</h3>
+            <p></p>
+            <a href="tel:+6285 242 700 383">+6285 242 700 383</a>
+        </div>
+        <div class="ca1">
+            <div class="icon"> <a href="mailto:ferdysudiago@gmail.com?subject=AC%20saya%20sedang%20bermasalah&body=Assalamu%27alaikum%20pak%21%20AC%20saya%20sedang%20bermasalah%0ANama%3A%0AAlamat%3A%0ANomor%20hp%3A%0AMerek%20AC%3A%0AKendala%3A"><i class="fas fa-envelope-open-text"></i></a></div>
+            <h3>Email</h3>
+            <p></p>
+            <a href="mailto:ferdysudiago@gmail.com?subject=AC%20saya%20sedang%20bermasalah&body=Assalamu%27alaikum%20pak%21%20AC%20saya%20sedang%20bermasalah%0ANama%3A%0AAlamat%3A%0ANomor%20hp%3A%0AMerek%20AC%3A%0AKendala%3A">Ferdysudiago@gmail.com</a>
+        </div>
+        </div>
+        </div>
+   </section>
+   
+
+
+
+        <section class="alamat">
+        <div class="containerF">
+        <h4>ALAMAT</h4>
+
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d994.5717420802141!2d119.88960652919864!3d-4.357183399802061!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d95f74d56fe972b%3A0xce960ca541b6527a!2sService%20AC%20dan%20elektronik!5e0!3m2!1sid!2sid!4v1621394383975!5m2!1sid!2sid"
+             width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+
+        </section>
+
+        <!--footer-->
+        <footer>
+            <div class="containerF">
+                <small>Copyright &copy; 2021 - SarinA. All Rights Reserved</small>
+            </div>
+
+        </footer>
+
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $(".bgloader").hide();
+        })
+        </script>
+    </body>
+</html>
